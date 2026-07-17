55e18727b6025203d515f2f1ff7287e16b9507c0
soufu <c1417005@st.kanazawa-it.ac.jp>
2026-07-17 17:49:53 +0900

    アプリ開発用に整えた。

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+origins = [
+    ""
+]
+
+#セキュリティ面のコード。Reactアプリと通信の際の権限関係。
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def Hello():
+    return{"Hello":"World!"}
